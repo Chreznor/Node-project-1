539bd8bdfd3a92f7480c01bc05d78aa14036312a
Chreznor <chechur@bk.ru>
2017-09-05 18:13:45 +0300

    From now on stores can be 'hearted'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const User = mongoose.model('User');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
@@ -140,9 +141,25 @@ exports.mapStores = async (req, res) => {
 
 exports.mapPage = (req, res) => {
   res.render('map', { title: 'Map'});
+};
+
+
+exports.heartStore = async (req, res) => {
+  const hearts = req.user.hearts.map(obj => obj.toString());
+  // the operator that is responsible for finding and updating a user needs to be a variable
+  const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+  const user = await User.findByIdAndUpdate(req.user._id, 
+                    {[operator]: {hearts: req.params.id}},
+                    { new: true}
+  );
+  res.json(user);
+};
+
+exports.heartsPage = async (req, res) => {
+  const stores = await Store.find({});
+  res.render('hearts', {title: 'Hearts', stores: stores});
+  // res.render('hearts', {title: 'Hearts'});
 }
-
-
 
 
 //end
