7d6ec795365323a149b40a9d7d5faa681cfd330b
Chreznor <chechur@bk.ru>
2017-09-06 14:42:55 +0300

    Implemented the functions to display reviews in stores

--- models/Store.js
+++ models/Store.js
@@ -38,6 +38,9 @@ const storeSchema = new mongoose.Schema({
     ref: 'User',
     required: 'You must supply an author'
   }
+}, {
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}
 });
 
 // Here we define our indexes
@@ -73,6 +76,14 @@ storeSchema.statics.getTagsList = function() {
     { $group: { _id: '$tags', count: { $sum: 1} }},
     { $sort: { count: -1}}
   ]);
-}
+};
+
+//it finds reviews where the store's ID property is equal to the review's store property
+
+storeSchema.virtual('reviews', {
+  ref: 'Review', // what model to link?
+  localField: '_id', // which field on the store?
+  foreignField: 'store' // which field on the review?
+});
 
 module.exports = mongoose.model('Store', storeSchema);
